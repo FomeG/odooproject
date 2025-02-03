6cb3ecb944fcfc5020c57333bce517f686c8b30b
Nguyen Hieu <99974579+nguyenhieuxyz@users.noreply.github.com>
2025-02-03 16:17:00 +0700

    Update vehicle_info.py

--- vehicle_info.py
+++ vehicle_info.py
@@ -4,6 +4,9 @@ class VehicleInfo(models.Model):
     _name = 'vehicle.info'
     _description = 'Vehicle Info'
 
+    owner = field.Char(string='Chủ xe')
+    email = field.Char(string='Email')
+    total_money = field.Float(string='Doanh thu khách hàng')
     plate_number = field.Integer(string='Biển số xe')
     type = field.Char(string='Loại xe')
     trademark = field.Char(string='Dòng xe')
