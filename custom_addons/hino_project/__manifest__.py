4957ea84c3dfe5677af4bc6a2e7d52311f755c43
Lê Chính Đại <140992360+RelliZisme98@users.noreply.github.com>
2025-02-03 17:08:02 +0700

    add header
    add header

--- custom_addons/hino_project/__manifest__.py
+++ custom_addons/hino_project/__manifest__.py
@@ -3,10 +3,11 @@
     'version': '17.0.1.0',
     'author': 'Le Chinh Dai',
     'license': 'LGPL-3',
-    'depends': ['base', 'repair', 'appointment'],
+    'depends': ['base', 'repair', 'appointment', 'fleet'],
     'data': [
         'views/repair_quotations.xml',
         'views/repair_order_view.xml',
+        # 'views/repair_order_header_views.xml',
         'views/menus.xml',
     ],
     'installable': True,
