43ece2b7161a7d453bcb5e3fb84a3fef77b64622
Lê Chính Đại <140992360+RelliZisme98@users.noreply.github.com>
2025-02-03 14:11:37 +0700

    add header chu xe
    add header chu xe

---
+++ custom_addons/hino_project/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Hino_Project',
+    'version': '17.0.1.0',
+    'author': 'Le Chinh Dai',
+    'license': 'LGPL-3',
+    'depends': ['base', 'repair', 'appointment'],
+    'data': [
+        'views/repair_quotations.xml',
+        'views/repair_order_view.xml',
+        'views/menus.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
