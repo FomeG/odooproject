ad4143c7aaae413e83140f79d2b4b7498de74cc4
duongtuIT <duong48691592@gmail.com>
2025-02-03 14:21:09 +0700

    v2

--- models/repair_quotations.py
+++ models/repair_quotations.py
@@ -2,20 +2,19 @@ from odoo import api, fields, models
 
 
 class RepairQuotations(models.Model):
-    _name = 'repair.quotations'
-    _description = 'Repair Quotations'
+    _inherit = 'repair.order'
 
     #Thông tin header chứng từ - Thông tin chung
-    x_customer_feedback = fields.Char(string='Customer Feedback', help='Ý kiến khách hàng')
+    x_customer_feedback = fields.Char(string='Ý kiến khách hàng', help='Ý kiến khách hàng')
     x_notes = fields.Char(string='Notes', help='Ghi chú/Lưu ý/Khuyến nghị')
-    x_content = fields.Char(string='Content', help='Nội dung')
+    x_content = fields.Char(string='Nội dung', help='Nội dung')
 
     x_pricelist_id = fields.Many2one(
         'product.pricelist',
-        string='Price List',
+        string='Bảng giá',
         help='Bảng giá được liên kết')
 
-    x_total = fields.Monetary(string='Total', help='Thành tiền')
-    x_tax_amount = fields.Monetary(string='Tax Amount', help='Tiền thuế')
-    x_total_amount = fields.Monetary(string='Total Amount', help='Tổng')
+    x_total = fields.Monetary(string='Thành tiền', help='Thành tiền')
+    x_tax_amount = fields.Monetary(string='Tiền thuế', help='Tiền thuế')
+    x_total_amount = fields.Monetary(string='Tổng', help='Tổng')
 
