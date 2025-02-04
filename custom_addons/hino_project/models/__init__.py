401bb6b0a80a5400b77ffc65e843302770d977a6
FomeG <nghiavuong04@gmail.com>
2025-02-04 15:10:25 +0700

    Update Header Buttons

--- custom_addons/hino_project/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import repair_order, repair_order_header, repair_quotations
--- custom_addons/service_info/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import tab_service_sparepart_list
-from . import models
-from . import tab_service_info_document
-from . import tab_service_info_worklist_service
